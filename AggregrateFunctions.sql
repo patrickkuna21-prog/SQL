c77b9760f907ea86384208a7d5d0ce8a10c756f8
patrickkuna21-prog <patrickkuna21@gmail.com>
2026-02-03 23:27:27 +0100

    AggregrateFunctions

---
+++ AggregrateFunctions.sql
@@ -0,0 +1,5 @@
+SELECT SUM(salary) AS Total_Salary FROM Employees;
+SELECT AVG(salary) AS Average_Salary FROM Employees;
+SELECT COUNT(DISTINCT department) AS Total_department FROM Employees;
+SELECT MIN(salary) AS Minimum_Salary FROM Employees;
+SELECT MAX(salary) AS Maximum_Salary FROM Employees;
